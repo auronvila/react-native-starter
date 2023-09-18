e0957b2df35a97de62b1f63c93a7de0d0967bf87
Auron <98396887+auronvila@users.noreply.github.com>
2023-09-18 19:40:43 +0300

    updates

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -3,11 +3,6 @@ import {Button, Linking, StyleSheet, Text, View} from 'react-native'
 export default function ComponentScreen() {
   const greeting = <Text style={styles.testStyle}>Hello World from variable</Text>
 
-  const openExternalLink = () => {
-    const url = 'https://www.bloxima.com';
-    Linking.openURL(url)
-      .catch((err) => console.error('An error occurred: ', err));
-  };
 const name = 'Auron'
 
 
@@ -16,7 +11,6 @@ const name = 'Auron'
       <Text style={styles.textStyle}>Getting started with react native</Text>
       <Text style={styles.testStyle}>my name is {name}</Text>
       {greeting}
-      <Button title={'press to go to google'} onPress={openExternalLink}/>
     </View>
   )
 }
